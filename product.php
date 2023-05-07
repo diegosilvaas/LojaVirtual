92db1b3ef2df52fe74735c0a6b7829855914b682
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-07 16:50:39 -0400

    refatorando produto

--- product.php
+++ product.php
@@ -1,56 +1,50 @@
 <?php
 
-class Product 
+class Product
 {
 
-    private int $id;
-    private string $name;
-    private int $price;
-    private int $quantity;
+  private int $id;
+  private string $name;
+  private int $price;
+  private int $quantity;
 
-    public function setId(int $id)
-    {
-        $this->id = $id;
-    }
+  public function setId(int $id)
+  {
+    $this->id = $id;
+  }
 
-    public function setName(string $name)
-    {
-        $this->name = $name;
-    }
+  public function setName(string $name)
+  {
+    $this->name = $name;
+  }
 
-    public function setPrice(int $price)
-    {
-        $this->price = $price;
-    }
+  public function setPrice(int $price)
+  {
+    $this->price = $price;
+  }
 
-    public function setQuantity(int $quantity)
-    {
-        $this->quantity = $quantity;
-    }
+  public function setQuantity(int $quantity)
+  {
+    $this->quantity = $quantity;
+  }
 
-    public function getId()
-    {
-        return $this->id;
+  public function getId()
+  {
+    return $this->id;
+  }
 
-    }
+  public function getName()
+  {
+    return $this->name;
+  }
 
-    public function getName()
-    {
-        return $this->name;
+  public function getPrice()
+  {
+    return $this->price;
+  }
 
-    }
-
-    public function getPrice()
-    {
-        return $this->price;
-
-    }
-
-    public function getQuantity()
-    {
-        return $this->quantity;
-
-    }
-
-
+  public function getQuantity()
+  {
+    return $this->quantity;
+  }
 }
