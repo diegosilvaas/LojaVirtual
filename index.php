2ba067db01a540c075f43bdf81a95cddc58f1e8f
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-07 16:51:05 -0400

    refatorando index

--- index.php
+++ index.php
@@ -6,26 +6,24 @@ require 'Product.php';
 session_start();
 
 $products = [
-    1 => ['id' => '1', 'name' => 'geladeira', 'price' => 3000, 'quantity' => 1],
-    2 => ['id' => '2', 'name' => 'mouse', 'price' => 200, 'quantity' => 1],
-    3 => ['id' => '3', 'name' => 'teclado', 'price' => 300, 'quantity' => 1],
-    4 => ['id' => '4', 'name' => 'monitor', 'price' => 1500, 'quantity' => 1],
+  1 => ['id' => 1, 'name' => 'geladeira', 'price' => 1000, 'quantity' => 1],
+  2 => ['id' => 2, 'name' => 'mouse', 'price' => 100, 'quantity' => 1],
+  3 => ['id' => 3, 'name' => 'teclado', 'price' => 10, 'quantity' => 1],
+  4 => ['id' => 4, 'name' => 'monitor', 'price' => 5000, 'quantity' => 1],
 ];
 
-if (isset($_GET['add'])) {
-    $id = strip_tags($_GET['id']);
-    $productInfo = $products[$id];
-    $product = new Product;
-    $product->setId($productInfo['id']);
-    $product->setName($productInfo['name']);
-    $product->setPrice($productInfo['price']);
-    $product->setQuantity($productInfo['quantity']);
 
+if (isset($_GET['id'])) {
+  $id = strip_tags($_GET['id']);
+  $productInfo = $products[$id];
+  $product = new Product;
+  $product->setId($productInfo['id']);
+  $product->setName($productInfo['name']);
+  $product->setPrice($productInfo['price']);
+  $product->setQuantity($productInfo['quantity']);
 
-
-    $cart = new Cart;
-    $cart->add($product);
-
+  $cart = new Cart;
+  $cart->add($product);
 }
 
 var_dump($_SESSION['cart'] ?? []);
@@ -34,24 +32,21 @@ var_dump($_SESSION['cart'] ?? []);
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        </head>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
+<body>
+  <a href="/mycart.php">Go to cart</a>
+  <ul>
+    <li>Geladeira <a href="?id=1">Add</a> R$ 3000</li>
+    <li>Mouse <a href="?id=2">Add</a> R$ 200</li>
+    <li>Teclado <a href="?id=3">Add</a> R$ 3000</li>
+    <li>Monitor <a href="?id=4">Add</a> $$ 1500</li>
+  </ul>
+</body>
 
-        <body>
-            <a href="/myCart.php"> Go to cart</a>
-            <ul>
-                <li> Geladeira <a href="?id=1">Add</a>R$3000</li>
-                <li> Mouse <a href="?id=1">Add</a>R$200</li>
-                <li> Teclado <a href="?id=1">Add</a>R$300</li>
-                <li> Monitor <a href="?id=1">Add</a>R$1500</li>
-              
-
-            </ul>
-
-        </body>
 </html>
